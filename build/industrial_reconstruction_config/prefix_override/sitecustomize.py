e1ced3150ca9726652f0397421fa737740ccad55
Judith Rodriguez <judith_rodriguezchacon@mines.edu>
2026-06-11 16:34:04 -0600

    updated launch file to decrease depth

---
+++ build/industrial_reconstruction_config/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/Users/judithrodriguez/.pyenv/versions/3.14.5':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/Users/judithrodriguez/Documents/CSCI370_FieldSession/Mesh_Reconstruction/install/industrial_reconstruction_config'
